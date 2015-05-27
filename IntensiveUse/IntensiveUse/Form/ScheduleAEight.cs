009e1d6251a4046be8ad23258aab065f352a63fb
wangjianlong <810844480@qq.com>
2015-05-27 19:15:01 +0800

    准备附表1A8

---
+++ IntensiveUse/IntensiveUse/Form/ScheduleAEight.cs
@@ -0,0 +1,19 @@
+﻿using IntensiveUse.Helper;
+using IntensiveUse.Manager;
+using NPOI.SS.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntensiveUse.Form
+{
+    public class ScheduleAEight:ISchedule
+    {
+        public IWorkbook Write(string FilePath, ManagerCore Core, int Year, string City, string Distict)
+        {
+            IWorkbook workbook = ExcelHelper.OpenWorkbook(FilePath);
+            return workbook;
+        }
+    }
+}
